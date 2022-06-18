5a96caf338365a48e79083c2b306722f465298ec
Andres Zapata <me@azapata.io>
2022-06-18 11:20:36 +0200

    fix: :bug: Add components as parent path of story

--- src/templates/stories.tsx
+++ src/templates/stories.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { COMPONENT_NAME_ } from './COMPONENT_NAME_.component'
 
 export default {
-  title: 'Path/To/COMPONENT_NAME_',
+  title: 'Components/COMPONENT_NAME_',
   component: COMPONENT_NAME_,
 }
 
